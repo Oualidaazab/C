560316c175c9534a3f3c49fa98863de188060fe9
OUALID AAZAB <aazabwalid20@gmail.com>
2026-09-22 07:41:49 -0700

    strdup.c function in c

---
+++ ft_strdup.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+int str_len(char *string)
+{
+    int len = 0; 
+    while (string[len])
+    {
+        len++;
+    }
+    return(len);
+}
+char *ft_strdup(char *string)
+{
+        int i  = 0;
+        char *dest ; 
+        int len  = str_len(string);
+        dest  =malloc(sizeof(char) *(len+1));
+        if (!dest)
+        {
+            return(0);
+        }
+       else{
+        while (string[i] != '\0')
+        {
+            dest[i] = string[i];
+            i++;
+        }
+        dest[i] = '\0';
+        }
+        i =0 ;
+        while(dest[i] != '\0')
+        {
+            write(1,&dest[i],1);
+            i++;
+        }
+}
+
+int main(void)
+{
+    char string[] = "hello user";
+    ft_strdup(string);
+}
