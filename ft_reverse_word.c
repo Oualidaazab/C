10865b9a06d4c44787e8882d406707a18c9c138c
OUALID AAZAB <aazabwalid20@gmail.com>
2026-09-25 12:19:59 +0000

    revers world

---
+++ ft_reverse_word.c
@@ -0,0 +1,60 @@
+
+
+/*
+** Reverse a String
+**
+** Write a function that takes a string as input and reverses
+** the order of its characters.
+**
+** Example:
+**     Input:  "hello world"
+**     Output: "dlrow olleh"
+**
+** The function should traverse the string and return the
+** reversed version of the original string.
+*/
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_string.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OUAILD AAZAB <username@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/25 12:00:00 by oualid aazab      #+#    #+#             */
+/*   Updated: 2026/09/25 12:00:00 by oualid aaza      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>  
+
+int ft_strlen(char *string)
+{
+    int i  = 0 ;  
+    while(string[i] != '\0')
+    {
+        i++; 
+    }
+    return(i);
+}
+
+void ft_revers(char *string)
+{
+      int len  = ft_strlen(string);
+      while(len--)
+      {
+        write(1,&string[len],1); 
+      }
+}
+int main(int ac , char **av)
+{
+    if(ac >= 2)
+    {
+
+        ft_revers(av[1]);
+    }
+    else
+    {
+        write(1,"\n",1);
+    }
+}
